1e92bf11c07b2d5a50e584172afa3f029191296a
Devendra Tewari <devendra.tewari@gmail.com>
2020-09-09 12:30:19 -0300

    update pandoc docker image

--- run-docker.sh
+++ run-docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -i -t -v `pwd`:/workdir -w /workdir tewarid/pandoc:1.0 bash run-pandoc.sh
+docker run --rm -i -t -v `pwd`:/workdir -w /workdir --entrypoint ./run-pandoc.sh tewarid/pandoc:development
--- run-pandoc.sh
+++ run-pandoc.sh
@@ -1,5 +1,5 @@
-files=(front-matter.md slides.md)
+#!/bin/sh
 # create standalone revealjs presentation
-pandoc --lua-filter filter.lua --slide-level=2 -o slides.html -t revealjs -s -V minScale=0.2 -V maxScale=1.5 -V width=\"100%\" -V height=\"150%\" -V margin=0.05 ${files[@]}
+pandoc --lua-filter filter.lua --slide-level=2 -o slides.html -t revealjs -s -V minScale=0.2 -V maxScale=1.5 -V width=\"100%\" -V height=\"150%\" -V margin=0.05 front-matter.md slides.md
 # create PDF slides using beamer
-pandoc --lua-filter filter.lua --slide-level=2 -o slides.pdf -t beamer ${files[@]}
+pandoc --lua-filter filter.lua --slide-level=2 -o slides.pdf -t beamer front-matter.md slides.md
